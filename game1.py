91098eeb958d82dd1928acd360c0bbd6ed596bc8
Wenjuan <wenjuan.zhang@ucdenver.edu>
2020-02-13 21:47:40 -0700

    Review and add some comments

--- game1.py
+++ game1.py
@@ -1,15 +1,15 @@
-import pygame, sys
+import pygame, sys            #when no sys, running no error, exit will give error
 from pygame.locals import *
 
 pygame.init()
-win = pygame.display.set_mode((400, 600))
+win = pygame.display.set_mode((400, 600))  #mode, not model
 pygame.display.set_caption('Hello World!')
 while True: # main game loop
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
-            sys.exit()
-    pygame.display.update()
+            sys.exit()          #??
+    #pygame.display.update()     #so far, no changes
 
 
 # import pygame
